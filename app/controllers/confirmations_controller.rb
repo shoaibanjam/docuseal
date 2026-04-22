cd6a769746a66c1f225d4c291a5df34d387cd518
Usman Sarwar <usman.sarwar@cybros.dev>
2026-04-22 13:01:31 +0500

    Handled case of  already confirmed  email

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,6 +3,23 @@
 class ConfirmationsController < Devise::ConfirmationsController
   around_action :with_browser_locale
 
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
+    elsif already_confirmed_error?(resource)
+      flash[:notice] = I18n.t('devise.confirmations.already_confirmed')
+      redirect_to new_user_session_path(
+        { user: { email: resource.email.presence } }.compact_blank.merge(redir: params[:redir], lang: params[:lang]).compact_blank
+      )
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_content) { render :new }
+    end
+  end
+
   def create
     email = params.dig(resource_name, :email).to_s.downcase
     user = resource_class.find_by(email:)
@@ -42,5 +59,11 @@ class ConfirmationsController < Devise::ConfirmationsController
     end
 
     redirect_to new_user_session_path(user: { email: email }, redir: params[:redir], lang: params[:lang])
+  end
+
+  private
+
+  def already_confirmed_error?(resource)
+    resource.errors.details.fetch(:email, []).any? { |error| error[:error] == :already_confirmed }
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -71,6 +71,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+  config.mailer_sender = ENV.fetch('AUTH_MAILER_FROM', ENV.fetch('SMTP_USERNAME', 'info@docuseal.com'))
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
